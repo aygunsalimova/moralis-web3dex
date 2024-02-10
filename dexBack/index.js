5462b21d471c461934e70807796be72ef2d6941a
Aygun Salimova <aygunselim2019@gmailcom>
2024-02-10 19:08:06 +0400

    inital

--- dexBack/index.js
+++ dexBack/index.js
@@ -9,7 +9,20 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/tokenPrice", async (req, res) => {
-  return res.status(200).json(usdPrices);
+
+  const {query} = req;
+
+  const responseOne = await Moralis.EvmApi.token.getTokenPrice({
+    address: query.addressOne
+  })
+  const responseTwo = await Moralis.EvmApi.token.getTokenPrice({
+    address: query.addressTwo
+  })
+
+  console.log(responseOne.raw)
+  console.log(responseTwo.raw)
+
+  return res.status(200).json({});
 });
 
 Moralis.start({
